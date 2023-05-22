60469cb7ab77595e5fa2c6529d3b975c373fc88c
JDM <shchuka_pasha@mail.ru>
2023-05-22 10:13:32 +0300

    feat: add menu component, add new variables

--- tracker/src/components/Layout/Layout.tsx
+++ tracker/src/components/Layout/Layout.tsx
@@ -2,6 +2,8 @@ import { FC, ReactNode } from 'react'
 
 import logo from 'assets/icons/logo.svg'
 
+import Menu from 'components/Menu'
+
 import styles from './Layout.module.scss'
 
 type LayotProps = {
@@ -11,9 +13,16 @@ type LayotProps = {
 const Layout: FC<LayotProps> = (props) => {
   return (
     <div className={styles.layout}>
-      <div className={styles.layout__logoBlock}>
-        <img className={styles.layout__logoBlock_logo} src={logo} alt="logo" />
-        <h3 className={styles.layout__logoBlock_title}>Hydrocult</h3>
+      <div className={styles.layout__leftbar}>
+        <div className={styles.leftbar__logoBlock}>
+          <img
+            className={styles.leftbar__logoBlock_logo}
+            src={logo}
+            alt="logo"
+          />
+          <h3 className={styles.leftbar__logoBlock_title}>Hydrocult</h3>
+        </div>
+        <Menu />
       </div>
       <div className={styles.layout__childrenBlock}>{props.children}</div>
     </div>
--- tracker/src/components/Menu/Menu.tsx
+++ tracker/src/components/Menu/Menu.tsx
@@ -1,7 +1,23 @@
 import { FC } from 'react'
 
+import { NAVIGATION_MENU } from './constants'
+
+import styles from './Menu.module.scss'
+
 const Menu: FC = () => {
-  return <div>Menu</div>
+  return (
+    <div className={styles.menu}>
+      <h3 className={styles.menu__title}>Menu</h3>
+      <div className={styles.menu__block}>
+        {NAVIGATION_MENU.map(({ label, icon, id }) => (
+          <p key={id} className={styles.menu__block_item}>
+            {icon}
+            {label}
+          </p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Menu
