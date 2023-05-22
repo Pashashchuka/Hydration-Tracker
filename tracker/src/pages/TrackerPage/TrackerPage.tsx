b5967bd7f493e363e263635b5d696d2a0baf5e15
JDM <shchuka_pasha@mail.ru>
2023-05-22 15:28:19 +0300

    feat: add head block to tracker page, add new variables

--- tracker/src/pages/TrackerPage/TrackerPage.tsx
+++ tracker/src/pages/TrackerPage/TrackerPage.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 
+import search from 'assets/icons/search.svg'
+
 import Layout from 'components/Layout'
 
 import styles from './TrackerPage.module.scss'
@@ -7,7 +9,25 @@ import styles from './TrackerPage.module.scss'
 const TrackerPage: FC = () => {
   return (
     <Layout>
-      <div className={styles.wrapper}>TrackerPage</div>
+      <div className={styles.wrapper}>
+        <div className={styles.wrapper__headBlock}>
+          <h2 className={styles.wrapper__headBlock_title}>
+            Welcome back <span className={styles.title__word}>Mathew!</span>
+          </h2>
+          <div className={styles.wrapper__headBlock_searchBlock}>
+            <img
+              className={styles.searchBlock__icon}
+              src={search}
+              alt="search"
+            />
+            <input
+              className={styles.searchBlock__search}
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
